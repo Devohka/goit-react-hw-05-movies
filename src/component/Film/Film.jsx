d65c151d859cb186103b37bf105e2ee740dc09ed
Devohka <andr.fedak@gmail.com>
2025-01-11 12:40:34 +0200

    gh

--- src/component/Film/Film.jsx
+++ src/component/Film/Film.jsx
@@ -3,12 +3,14 @@ import { useParams } from "react-router-dom";
 
 
 export default function Film() {
+     const { id } = useParams(); 
+     console.log(id);
     const item = JSON.parse(localStorage.getItem("filmFull"));
-    const { id } = useParams();
+   
     console.log(item);
     // https://api.themoviedb.org/3/movie/{movie_id}/images
-    console.log(`https://api.themoviedb.org/3/movie/${item.id}/images${item.backdrop_path}`);
-    console.log(id);
+    // console.log(`https://api.themoviedb.org/3/movie/${item.id}/images${item.backdrop_path}`);
+   
     // https://api.themoviedb.org/3/movie/${id}&include_adult=false&language=en-US&api_key=a49a343936bdd37a7594fe7daf741bfa
     fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=a49a343936bdd37a7594fe7daf741bfa&language=en-US`)
         .then(data =>
